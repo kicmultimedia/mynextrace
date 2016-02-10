fd72519330698e6f1c2783b9ef8b649a14ec1e81
kevinc <kevin.cornwell@pennantplc.co.uk>
2016-02-10 07:36:44 +0000

    Removing the attempted display of the larger images on click.

--- platforms/android/assets/www/js/eventview.js
+++ platforms/android/assets/www/js/eventview.js
@@ -14,16 +14,6 @@ app.EventView = (function () {
 
         this.initialize = function () {
             this.render();
-            // Define a div wrapper for the view. The div wrapper is used to attach events.
-            this.$el = $('<div/>');
-            this.$el.on('click', '.small-pic', function () {
-                $(".large-pic-bg", this.$el).show();
-                $(".large-pic", this.$el).show();
-            });
-            this.$el.on('click', '.large-pic', function () {
-                $(".large-pic", this.$el).hide();
-                $(".large-pic-bg", this.$el).hide();
-            });
         };
 
         this.render = function () {
