457e3be86c668516291d26379b8adb0137b0b723
kevinc <kevin.cornwell@pennantplc.co.uk>
2016-03-15 09:09:24 +0000

    adding a temporary handlebar event handler to allow an equality check within handlebar templates. This is done with the following:
    {{#equal unicorns ponies }}
    That's amazing, unicorns are actually undercover ponies
    {{/equal}}

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -89,3 +89,13 @@ var app = {
         if (typeof fn === "function") fn(viewName, args);
     }
 };
+
+Handlebars.registerHelper('equal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
